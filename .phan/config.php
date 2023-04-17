a257923b65a6b515991df067ac330fdd2ca419ae
James D. Forrester <jforrester@wikimedia.org>
2023-04-17 16:35:49 -0400

    build: Switch phan to special library mode
    Bug: T334492
    Change-Id: If13f929755ef15f9e1d3c503a7d2dda3c1814466

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 $cfg['directory_list'][] = 'vendor/';
 
@@ -10,5 +10,7 @@ $cfg['exclude_analysis_directory_list'][] = 'vendor/';
 $cfg['suppress_issue_types'][] = 'PhanTypeMismatchArgumentInternal';
 $cfg['suppress_issue_types'][] = 'PhanTypeMismatchProperty';
 $cfg['suppress_issue_types'][] = 'PhanTypeMismatchPropertyProbablyReal';
+// Suppress PHP 8.0+ issue on PHP 7.4 compatibility code
+$cfg['suppress_issue_types'][] = 'PhanDeprecatedFunctionInternal';
 
 return $cfg;
